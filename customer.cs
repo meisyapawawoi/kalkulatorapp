a5796d3f8728b2221bec4f7f5495d03e2fc1299b
meisyapawawoi <61919304+meisyapawawoi@users.noreply.github.com>
2020-07-08 15:31:56 +0700

    Add files via upload

---
+++ customer.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProjectCustomer
+{
+    public class Customer
+    {
+        public string TambahCustomer { get; set; }
+		public string HapusCustomer { get; set; }
+		public string TampilCustomer { get; set; }
+
+    }
+}
